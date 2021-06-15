7c677932a7150a93f05c6800c106440732e46bbb
Katie Brent <kab369@cornell.edu>
2021-06-14 18:56:18 -0600

    countUniqueValues implementation

---
+++ countUniqueValues.js
@@ -0,0 +1,27 @@
+//implement a function called countUniqueValues which accepts a sorted array
+//and counts the unique values in the array.
+//there can be negative numbers in the array, but it will always be sorted
+
+function countUniqueValues (arr) {
+  if (arr.length < 2) {
+    return arr.length
+  }
+
+  let count = 1;
+
+  let prev = 0;
+  let current = 1;
+
+  while(current < arr.length) {
+    if(arr[current] === arr[prev]) {
+      current++
+    } else {
+      count++;
+      prev = current;
+      current++;
+    }
+  }
+
+  return count;
+
+}
