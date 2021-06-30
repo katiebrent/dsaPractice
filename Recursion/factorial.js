3dcdea0e010af6db0097620098d88df2d8fac2ba
Katie Brent <kab369@cornell.edu>
2021-06-30 14:50:01 -0600

    fixing factorial

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -1,7 +1,9 @@
 function factorial (num) {
-  if(num === 1) {
-    return num;
+  if(num === 1 || num === 0) {
+    return 1;
   }
 
-  return num * factorial(num)
+  return num * factorial(num - 1)
 }
+
+console.log(factorial(4));
