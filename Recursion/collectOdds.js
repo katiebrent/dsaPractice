4a6c8dff95719035df702b5535b22648ebed151a
Katie Brent <kab369@cornell.edu>
2021-06-30 14:35:33 -0600

    true recursion collect odds

--- Recursion/collectOdds.js
+++ Recursion/collectOdds.js
@@ -17,3 +17,22 @@ function collectOdds(nums) {
   return result;
 
 }
+
+//true recursion
+
+function collectOdds(arr) {
+  let newArr = [];
+
+  if(arr.length === 0) {
+    return newArr;
+  }
+
+  if(arr[0] % 2 !== 0) {
+    newArr.push(arr[0]);
+  }
+
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+  return newArr;
+
+
+}
